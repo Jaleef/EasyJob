790dd2d168e3524051329a00e438ae8d9316d143
闫佳乐 <jeffyan167@gmail.com>
2025-06-03 16:23:10 +0800

    导入react-router-dom工具，并且设置初始的四个路由

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react"
+import { Routes, Route, useNavigate } from "react-router-dom"
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+function RootRedirect() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate('/login');
+  }, [navigate]);
+
+  return null;
+}
 
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={<RootRedirect />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   )
 }
 
