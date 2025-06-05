173f880baaa4df414ff493310b079cf7d73d2d9d
crazykunkun <2912326974@qq.com>
2025-06-05 11:39:54 +0800

    添加了连续登录次数拦截

--- backend/src/main/java/com/software_test/controller/AccountController.java
+++ backend/src/main/java/com/software_test/controller/AccountController.java
@@ -7,20 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.software_test.pojo.Account;
 import com.software_test.service.impl.AccountServiceImpl;
 import com.software_test.pojo.Result;
-
+import com.software_test.utils.LoginAttemptService;
 import lombok.extern.slf4j.Slf4j;
-
+import jakarta.servlet.http.HttpServletRequest;
 
 
 @Slf4j
 @RestController
 public class AccountController {
-
+    @Autowired
+    private LoginAttemptService loginAttemptService;
     @Autowired
     private AccountServiceImpl accountService;
 
     @PostMapping("/login")
-    public Result login(@RequestBody Account account) {
+    public Result login(HttpServletRequest request,@RequestBody Account account) {
+        String ip = request.getRemoteAddr();
+        if (loginAttemptService.isBlocked(ip)) {
+            return Result.error("该IP登录过于频繁，请稍后再试");
+        }
         // 只调用service，具体逻辑交给service处理
         Result result = Result.success(accountService.login(account));
         return result;
